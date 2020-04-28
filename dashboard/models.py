c8f050804a6a7575c7402dbd738b517fffda94d9
teekshan04 <42838353+teekshan04@users.noreply.github.com>
2020-04-28 11:48:04 +0530

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -11,7 +11,7 @@ class report (models.Model):
     CPC = models.CharField(max_length=480)
     Country = models.CharField(max_length=50, default="", editable=False)
     Publication_number = models.CharField(max_length=40, default="", editable=False)
-    ##Publication_date = models.CharField(max_length=10, default="", editable=False)
+    Publication_date = models.CharField(max_length=10, default="", editable=False)
     Publication_Year = models.CharField(max_length=40,default="", editable=False)
     Earliest_publication = models.CharField(max_length=40,default="", editable=False)
     Family_number = models.CharField(max_length=80,default="", editable=False)
